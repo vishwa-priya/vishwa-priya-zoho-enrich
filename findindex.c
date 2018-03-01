c70d5feefbfccccf3d1b7648827e94b6da8137b9
vishwa-priya <31279723+vishwa-priya@users.noreply.github.com>
2018-03-01 16:10:19 +0530

    Update findindex.c

--- findindex.c
+++ findindex.c
@@ -6,34 +6,19 @@ int main()
     
    int n,i,j,c=0,index=-1;
    char a[200];
-   
    scanf("%d ",&n);
- 
    scanf("%[^\n]",a);
- 
    char *b=a;
- 
-        
+   
    while(sscanf(b,"%d%n",&i,&j)==1)
    {
-        
       if(i==n)
-   
-   {
-            
+      {
          index=c;
 	 break;
-   
-   }
-
+      }
       c++;
-   
-   b=b+j;
-
-
+      b=b+j;
    }
-    
    printf("%d",index);
- 
-
 }
