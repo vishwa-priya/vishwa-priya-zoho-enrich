c2b470776e474f80a040ac872794110ac2d5e3ff
vishwa-priya <31279723+vishwa-priya@users.noreply.github.com>
2018-03-01 16:08:14 +0530

    Add files via upload

---
+++ findindex.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+
+int main() 
+{
+    
+   int n,i,j,c=0,index=-1;
+   char a[200];
+   
+   scanf("%d ",&n);
+ 
+   scanf("%[^\n]",a);
+ 
+   char *b=a;
+ 
+        
+   while(sscanf(b,"%d%n",&i,&j)==1)
+   {
+        
+      if(i==n)
+   
+   {
+            
+         index=c;
+	 break;
+   
+   }
+
+      c++;
+   
+   b=b+j;
+
+
+   }
+    
+   printf("%d",index);
+ 
+
+}
