c0479737429b19816b410e51cde53d4157f7ffbe
vishwa-priya <31279723+vishwa-priya@users.noreply.github.com>
2018-03-07 19:50:22 +0530

    Update stringcompare.c

--- stringcompare.c
+++ stringcompare.c
@@ -1,93 +1,51 @@
 #include<stdio.h>
-
 #include<string.h>
-
 long int str_to_int(char s[])
-
 {
-    
-   int i=0,j;
-    
-   long int n=0;
-    
-   if(s[0]=='-')
-   {
-        
-      i=1;
-   }
-    
-   for(j=i;j<strlen(s);j++)
-
-   {
-        
-      n=n*10+s[j]-'0';
-    
-   }
-    
-   if(s[0]=='-')
-
-   {   
+    int i=0,j;
+    long int n=0;
+    if(s[0]=='-')
+    {
+       i=1;
+    }
+    for(j=i;j<strlen(s);j++)
+    {
+       n=n*10+s[j]-'0';
+    }
+    if(s[0]=='-')
+    {   
       return -n;
-   }
-  
- return n;
-    
-
+    }
+    return n;
 }
-
-
 int main() 
 {
-  
-   int n,i,j;
- 
-   char t[10];
-
-   scanf("%d",&n);
-
-   char a[n][10];
-  
-   for(i=0;i<n;i++)
-  
-   {
-      
-      scanf("%s",a[i]);
-
-   }
-
-   for(i=0;i<n;i++)
-
-   {
-      
+  int n,i,j;
+  char t[10];
+  scanf("%d",&n);
+  char a[n][10];
+  for(i=0;i<n;i++)
+  {
+     scanf("%s",a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
       long int x=str_to_int(a[i]);
       for(j=i+1;j<n;j++)
-   
-   {
-        
+      {
          long int y=str_to_int(a[j]);  
          if(x>y)     
          {
-              
             strcpy(t,a[i]);
-
             strcpy(a[i],a[j]);
-
             strcpy(a[j],t);
-
          }
-
       }
- 
    }
-
    for(i=0;i<n;i++)
-
    {
-  
-    printf("%s ",a[i]);
-
+      printf("%s ",a[i]);
    }
-
 }
      
    
